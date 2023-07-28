7890b1c57970d86ea82a793eda62dcbe02ad94eb
steeven-louk <steevenlouk.sl@gmail.com>
2023-07-28 22:30:56 +0200

    finition temporaire de la homepage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,48 @@
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-white bg-dark pt-3">
+      <div className="container mb-4">
+      <div className="row">
+          <div className="col-md-3" >
+            <h5 className="mb-3">hexashop</h5>
+            <ul className="nav">
+              <li><b>Address :</b> Paris Road</li>
+              <li><b>E-mail :</b> Exemple@gmail.com</li>
+              <li><b>Phone :</b> 012 678 985</li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+            <h5 className="fw-bold fs-6 text mb-3">Shopping and Categories</h5>
+            <ul className="nav">
+              <li>Men's Shopping</li>
+              <li>Women's Shopping</li>
+              <li>Kid's Shopping</li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+          <h5 className="fw-bold fs-6 mb-3">Useful Links</h5>
+            <ul className="nav">
+              <li>Homepage</li>
+              <li>About Us</li>
+              <li>Help</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="col-md-3">
+          <h5 className="fw-bold fs-6 mb-3">Help & Information</h5>
+            <ul className="nav">
+              <li>FAQS</li>
+              <li>Shopping Us</li>
+              <li>Tracking Id</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className="copy bg-black p-2 text-center">
+        <span>&copy;Copyright @ 2023</span>
+      </div>
+    </footer>
   )
 }
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,16 +1,116 @@
-import { Header } from "../../components/header/Header"
-import { Section } from "../../components/template/Section"
+import { Header } from "../../components/header/Header";
+import { Section } from "../../components/template/Section";
 import { kidLatest, menLatest, womenLatest } from "../../services/data";
 
-export const Home = () => {
+import explore1 from "../../assets/explore1.png"
+import explore2 from "../../assets/explore2.png"
+import explore3 from "../../assets/explore3.png"
+import explore4 from "../../assets/explore4.png"
 
+import social1 from "../../assets/social1.png"
+import social2 from "../../assets/social2.png"
+import social3 from "../../assets/social3.png"
+import social4 from "../../assets/social4.png"
+import social5 from "../../assets/social5.png"
+import social6 from "../../assets/social6.png"
+
+export const Home = () => {
   return (
     <div>
-        <Header/>
+      <Header />
 
-        <Section items={menLatest} sectionTitle="men's latest" sectionText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corporis odit odio cupiditate beatae illo."/>
-        <Section items={womenLatest} sectionTitle="women's latest" sectionText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corporis odit odio cupiditate beatae illo."/>
-        <Section items={kidLatest} sectionTitle="kid's latest" sectionText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corporis odit odio cupiditate beatae illo."/>
+      <Section
+        items={menLatest}
+        sectionTitle="men's latest"
+        sectionText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corporis odit odio cupiditate beatae illo."
+      />
+      <Section
+        items={womenLatest}
+        sectionTitle="women's latest"
+        sectionText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corporis odit odio cupiditate beatae illo."
+      />
+      <Section
+        items={kidLatest}
+        sectionTitle="kid's latest"
+        sectionText="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni corporis odit odio cupiditate beatae illo."
+      />
+
+      <section className="ourProduct py-4 shadow bg-white">
+
+        <div className="container">
+        <h3>Explore Our Products</h3>
+        <div className="row justify-content-between ">
+          <div className="col-md-6">
+            <p style={{ "fontSize":"14px" }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged.
+            </p>
+            <p style={{ "fontSize":"14px" }}>
+              {" "}
+              It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with
+              desktop publishing software like Aldus PageMaker including
+              versions of Lorem IpsuLorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged.
+            </p>
+            <p style={{ "fontSize":"14px" }}>
+              {" "}
+              It was popularised in the 1960s with the release of Letraset
+              sheets containing Lorem Ipsum passages, and more recently with
+              desktop publishing software like Aldus PageMaker including
+              versions of Lorem IpsuLorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged.
+            </p>
+          </div>
+          <div className="col-md-6">
+            <div className="d-flex flex-wrap gap-3 justify-content-end">
+              <div className="">
+                <img src={explore1} alt="" style={{ "width":"11em" , objectFit: "cover"}}/>
+              </div>
+              <div className="">
+              <img src={explore2} alt="" style={{ "width":"11em" , objectFit: "cover"}}/>
+              </div>
+              <div className="">
+              <img src={explore3} alt="" style={{ "width":"11em" , objectFit: "cover"}}/>
+              </div>
+              <div className="">
+              <img src={explore4} alt="" style={{ "width":"11em" , objectFit: "cover"}}/>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </section>
+
+      <section className="social container my-5">
+        <div className="title">
+          <h3>social media</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit!</p>
+        </div>
+
+        <div className="img-social d-flex gap-2">
+          <img src={social1} alt="" style={{ "width":"150px", "height":"250px", "objectFit":"cover" }} />
+          <img src={social2} alt=""  style={{ "width":"150px", "height":"250px", "objectFit":"cover" }} />
+          <img src={social3} alt=""  style={{ "width":"150px", "height":"250px", "objectFit":"cover" }} />
+          <img src={social4} alt=""  style={{ "width":"150px", "height":"250px", "objectFit":"cover" }} />
+          <img src={social5} alt=""  style={{ "width":"150px", "height":"250px", "objectFit":"cover" }} />
+          <img src={social6} alt=""  style={{ "width":"150px", "height":"250px", "objectFit":"cover" }} />
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
